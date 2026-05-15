2dc702dae08def63640af5d1b99e1a85ab94e355
vinecioos <vinicius.bueno@live.com>
2026-05-14 23:29:19 -0300

    refatoracao da calculadora

--- calculadora/testeCalculadora.java
+++ calculadora/testeCalculadora.java
@@ -1,16 +1,27 @@
 package calculadora;
 
-public class testeCalculadora{
+public class testeCalculadora {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         calculadora calc = new calculadora();
 
-        //Testes Rápidos
-        calc.calc(2, 3, "+"); //esperando 5
-        calc.calc(10, 4, "-"); //esperando 6
-        calc.calc(3, 5, "*"); //esperando 15
-        calc.calc(8, 2, "/"); //esperando 4
-        calc.calc(8, 0, "/"); //divisão por zero
-        calc.calc(5, 5, "x"); //operação inválida
+        // Testes rápidos
+        System.out.println(calc.calc(2, 3, "+"));
+        System.out.println(calc.calc(2, 3, "-"));
+        System.out.println(calc.calc(2, 3, "*"));
+        System.out.println(calc.calc(2, 3, "/"));
+
+        try {
+            System.out.println(calc.calc(8, 0, "/"));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            System.out.println(calc.calc(5, 5, "x"));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
